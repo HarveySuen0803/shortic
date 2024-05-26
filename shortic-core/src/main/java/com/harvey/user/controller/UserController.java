e4414c631723fa67d50a458fc58a2e263193b9b6
harvey <harveysuen0803@gmail.com>
2024-05-26 17:13:11 +0800

    style(user): add permission control to the User module

--- shortic-core/src/main/java/com/harvey/user/controller/UserController.java
+++ shortic-core/src/main/java/com/harvey/user/controller/UserController.java
@@ -1,15 +1,12 @@
 package com.harvey.user.controller;
 
 import com.harvey.common.result.Result;
-import com.harvey.user.domain.UserDo;
-import com.harvey.user.dto.UserDto;
 import com.harvey.user.service.UserService;
 import com.harvey.user.vo.UserVo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.BeanUtils;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -18,9 +15,11 @@ import org.springframework.web.bind.annotation.*;
  * @Date 2024-05-22
  */
 @Slf4j
-@RequiredArgsConstructor
+@PreAuthorize("hasAuthority('USER_R')")
+@EnableMethodSecurity
 @RestController
 @RequestMapping("/api/user/v1")
+@RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
