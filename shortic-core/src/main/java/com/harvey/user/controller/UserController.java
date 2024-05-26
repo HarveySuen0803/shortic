6b4c464e9677533770663f4bad968d3fcabcee5a
harvey <harveysuen0803@gmail.com>
2024-05-26 17:16:10 +0800

    style(user): adjust the order of annotations

--- shortic-core/src/main/java/com/harvey/user/controller/UserController.java
+++ shortic-core/src/main/java/com/harvey/user/controller/UserController.java
@@ -4,22 +4,23 @@ import com.harvey.common.result.Result;
 import com.harvey.user.service.UserService;
 import com.harvey.user.vo.UserVo;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author harvey
  * @Email harveysuen0803@gmail.com
  * @Date 2024-05-22
  */
-@Slf4j
-@PreAuthorize("hasAuthority('USER_R')")
-@EnableMethodSecurity
 @RestController
 @RequestMapping("/api/user/v1")
+@PreAuthorize("hasAuthority('USER_R')")
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class UserController {
     private final UserService userService;
 
